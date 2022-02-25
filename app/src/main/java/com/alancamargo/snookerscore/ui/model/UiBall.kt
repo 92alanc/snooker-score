b4f72524f900dbdaa5209e3c8dd56d0a754e0799
Alan Camargo <92alanc@protonmail.com>
2022-02-25 11:00:17 -0300

    Updating colours

--- app/src/main/java/com/alancamargo/snookerscore/ui/model/UiBall.kt
+++ app/src/main/java/com/alancamargo/snookerscore/ui/model/UiBall.kt
@@ -5,13 +5,13 @@ import com.alancamargo.snookerscore.R
 
 enum class UiBall(@ColorRes val colour: Int) {
 
-    CUE_BALL(colour = R.color.white),
-    RED(colour = R.color.red),
-    YELLOW(colour = R.color.yellow),
-    GREEN(colour = R.color.green),
-    BROWN(colour = R.color.brown),
-    BLUE(colour = R.color.blue),
-    PINK(colour = R.color.pink),
-    BLACK(colour = R.color.black)
+    CUE_BALL(colour = R.color.ball_white),
+    RED(colour = R.color.ball_red),
+    YELLOW(colour = R.color.ball_yellow),
+    GREEN(colour = R.color.ball_green),
+    BROWN(colour = R.color.ball_brown),
+    BLUE(colour = R.color.ball_blue),
+    PINK(colour = R.color.ball_pink),
+    BLACK(colour = R.color.ball_black)
 
 }
