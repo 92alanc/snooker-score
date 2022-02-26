f1e8318ddb940cfa9746dffd78fa4f0edeeed29c
Alan Camargo <92alanc@protonmail.com>
2022-02-26 17:41:21 -0300

    Write missing test

--- app/src/test/java/com/alancamargo/snookerscore/domain/usecase/playerstats/UpdatePlayerStatsWithMatchResultUseCaseTest.kt
+++ app/src/test/java/com/alancamargo/snookerscore/domain/usecase/playerstats/UpdatePlayerStatsWithMatchResultUseCaseTest.kt
@@ -1,12 +1,36 @@
 package com.alancamargo.snookerscore.domain.usecase.playerstats
 
+import app.cash.turbine.test
+import com.alancamargo.snookerscore.domain.repository.PlayerStatsRepository
+import com.alancamargo.snookerscore.testtools.getPlayerStats
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 class UpdatePlayerStatsWithMatchResultUseCaseTest {
 
+    private val mockRepository = mockk<PlayerStatsRepository>()
+    private val useCase = UpdatePlayerStatsWithMatchResultUseCase(mockRepository)
+
     @Test
-    fun `invoke should update player stats`() {
-        // TODO: implement
+    fun `invoke should update player stats`() = runBlocking {
+        every { mockRepository.addOrUpdatePlayerStats(any()) } returns flow { emit(Unit) }
+        val playerStats = getPlayerStats()
+
+        val result = useCase.invoke(playerStats)
+
+        val expected = playerStats.copy(matchesWon = playerStats.matchesWon + 1)
+        verify { mockRepository.addOrUpdatePlayerStats(expected) }
+
+        result.test {
+            awaitItem()
+            awaitComplete()
+        }
     }
 
 }
