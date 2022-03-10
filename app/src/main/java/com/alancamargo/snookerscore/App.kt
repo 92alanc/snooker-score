a4981abf0588ca2d9153692f2616340b7c9a5dd5
Alan Camargo <92alanc@protonmail.com>
2022-03-10 16:13:17 -0300

    Add Smaato SDK

--- app/src/main/java/com/alancamargo/snookerscore/App.kt
+++ app/src/main/java/com/alancamargo/snookerscore/App.kt
@@ -2,6 +2,9 @@ package com.alancamargo.snookerscore
 
 import android.app.Application
 import com.alancamargo.snookerscore.core.arch.di.KoinAppDeclarationProvider
+import com.smaato.sdk.core.Config
+import com.smaato.sdk.core.SmaatoSdk
+import com.smaato.sdk.core.log.LogLevel
 import org.koin.core.context.GlobalContext.startKoin
 
 @Suppress("Unused")
@@ -10,6 +13,16 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(KoinAppDeclarationProvider.provideAppDeclaration(app = this))
+        initialiseSmaato()
+    }
+
+    private fun initialiseSmaato() {
+        val config = Config.builder()
+            .setLogLevel(LogLevel.ERROR)
+            .setHttpsOnly(true)
+            .build()
+
+        SmaatoSdk.init(this, config, BuildConfig.SMAATO_PUBLISHER_ID)
     }
 
 }
